cf89d21f13f1e30345b27492c4b5a8323c93d373
HafizMustaqim <84282907+HafizMustaqim@users.noreply.github.com>
2023-10-11 04:50:07 +0800

    second commit

---
+++ .history/backend/server_20231011030850.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+//express app
+const app = express();
+
+app.get
+
+//listen for requests
+app.listen(4000, () => {
+    console.log('listening on port 4000');
+})
