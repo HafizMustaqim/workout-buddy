2fcf431b682a8f53c281958dc7e978c2961cce7d
HafizMustaqim <84282907+HafizMustaqim@users.noreply.github.com>
2023-10-25 02:52:49 +0800

    fetch data
    need to fix

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'react'
 
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null)
 
     useEffect(() => {
-        const [workouts, setWorkouts] = useState(null)
-
         const fetchWorkouts = async () => {
-            const response = await fetch('http://localhost:4000/api/workouts')
+            const response = await fetch('/api/workouts')
             const json = await response.json()
 
             if (response.ok) {
